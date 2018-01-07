8795992d3550ebe8a23e98af09d8927a7dcff570
liuzekun <642338578@qq.com>
2018-01-07 18:37:39 +0800

    添加样式

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,17 +3,24 @@ require('styles/App.scss');
 
 import React from 'react';
 
-// let yeomanImage = require('../images/yeoman.png');
-// let imageDatas = require('../data/imageDatas.json');
+//获取图片的相关数据
+let imageDatas = require('../data/imageDatas.json');
+
+//利用自执行函数，将图片名信息转成图片URL路径信息
+imageDatas = (function getImageURL(imageDatasArr){
+  imageDatasArr.map((item) => {
+    item.imageURL = require('../images/'+item.fileName);
+  });
+  return imageDatasArr;
+})(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        {/*<img src={yeomanImage} alt="Yeoman Generator" />*/}
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-        <div className="notice">hello world!</div>
-      </div>
+      <section className="stage">
+        <section className="img-sec"></section>
+        <nav className="controller-nav"></nav>
+      </section>
     );
   }
 }
